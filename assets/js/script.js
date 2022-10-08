b0c96ce499f4d7de51857fc4c0df8b1c7793a706
Adam777 <Adam777-5867@outlook.com>
2022-10-08 12:42:19 +0000

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 const projectName = 'survey-form';
 localStorage.setItem('example_project', 'Survey Form');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	document.querySelector('#survey-form').addEventListener('submit', (event2) => {
+		event2.preventDefault();
+		alert('Unavailable.');
+	});
+});
